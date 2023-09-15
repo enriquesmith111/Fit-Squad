6aa75c52d3df0dde0deeee214ccd99dc34aee3fa
Enrique Smith Dean <enriquesmith111@gmail.com>
2023-09-15 04:46:21 +0100

    added join event functionality

--- app/controllers/event_participants_controller.rb
+++ app/controllers/event_participants_controller.rb
@@ -1,15 +1,32 @@
 class EventParticipantsController < ApplicationController
+  
+  def event_path(event)
+    # event_id is the ID of the event
+    event_id = event.id
+  
+    # Generate the path to the event page
+    path = "/events/#{event_id}"
+  
+    # Return the path
+    return path
+  end
+
   def index
-    @event_participant = EventParticpant.all
+    @event_participants = EventParticipant.all
   end
 
   def show
-    @event_participant = EventParticipant.find(params[:id])
-    if @event_participant.user_id == current_user.id
-      @group_participant.destroy
-      redirect_to groups_path, notice: "You have left the group."
+    @event = Event.find(params[:event_id])
+    if @event.present?
+      @event_participant = EventParticipant.find(params[:id])
+      if @event_participant.user_id == current_user.id
+        @event_participant.destroy
+        redirect_to event_path(@event), notice: "You have left the event."
+      else
+        redirect_to event_path(@event), alert: "You are not authorized to leave this event."
+      end
     else
-      redirect_to groups_path, alert: "You are not authorized to leave this group."
+      redirect_to events_path, alert: "Event not found."
     end
   end
 
@@ -21,7 +38,7 @@ class EventParticipantsController < ApplicationController
     event_id = params[:event_participant][:event_id] # Adjust this based on your form field name
 
     @event = Event.find(event_id)
-    @event_participant = eventParticipant.new(user_id: current_user.id, event_id:)
+    @event_participant = EventParticipant.new(user_id: current_user.id, event_id: event_id)
 
     if @event_participant.save
       redirect_to event_path(@event), notice: "Successfully joined the event!"
@@ -39,5 +56,20 @@ class EventParticipantsController < ApplicationController
     else
       render 'events/show' # Render the show page with errors if the update fails
     end
+  end
+
+  def destroy
+    @event_participant = EventParticipant.find(params[:id])
+  
+    if current_user == @event_participant.user
+      @event_participant.destroy
+      redirect_to event_path(@event), notice: "Successfully left the event!"
+    else
+      redirect_to event_path(@event), alert: "You can't leave this event."
+    end
+  end
+
+  def event_participant_params
+    params.require(:event_participant).permit(:user_id, :event_id)
   end
 end
